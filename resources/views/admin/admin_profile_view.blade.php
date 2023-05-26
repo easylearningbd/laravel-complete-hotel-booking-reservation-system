59f667d23954525cd292077aa2b13b9889da0e3b
easylearningbd <elebdsoft@gmail.com>
2023-05-27 03:50:18 +0600

    21 Admin Profile & Image Update Part 4

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -62,6 +62,10 @@
 							</div>
     <div class="col-lg-8">
         <div class="card">
+
+            <form action="{{ route('admin.profile.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+
             <div class="card-body">
                 <div class="row mb-3">
                     <div class="col-sm-3">
@@ -121,10 +125,12 @@
                 <div class="row">
                     <div class="col-sm-3"></div>
                     <div class="col-sm-9 text-secondary">
-                        <input type="button" class="btn btn-primary px-4" value="Save Changes" />
+                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
                     </div>
                 </div>
             </div>
+        </form>
+
         </div>
             
 
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware(['auth','roles:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 
 }); // End Admin Group Middleware 
 
