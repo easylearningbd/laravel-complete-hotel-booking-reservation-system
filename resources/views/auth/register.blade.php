8a322be37e689ed1744a3ec1859767304f84eae1
easylearningbd <elebdsoft@gmail.com>
2023-05-30 00:23:25 +0600

    29 Frontend Template Register Page Setup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,85 @@
-<x-guest-layout>
+@extends('frontend.main_master')
+@section('main')
+
+    <!-- Inner Banner -->
+    <div class="inner-banner inner-bg10">
+        <div class="container">
+            <div class="inner-title">
+                <ul>
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li><i class='bx bx-chevron-right'></i></li>
+                    <li>Sign Up</li>
+                </ul>
+                <h3>Sign Up</h3>
+            </div>
+        </div>
+    </div>
+    <!-- Inner Banner End -->
+
+    <!-- Sign Up Area -->
+    <div class="sign-up-area pt-100 pb-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="user-all-form">
+                        <div class="contact-form">
+                            <div class="section-title text-center">
+                                <span class="sp-color">Sign Up</span>
+                                <h2>Create an Account!</h2>
+                            </div>
+
     <form method="POST" action="{{ route('register') }}">
-        @csrf
+      @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        <div class="row">
+            <div class="col-lg-12 ">
+                <div class="form-group">
+                    <input type="text" name="name" id="name" class="form-control" required data-error="Please enter your Username" placeholder="Username">
+                </div>
+            </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="col-lg-12">
+                <div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control" required data-error="Please enter email" placeholder="Email">
+                </div>
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <div class="col-lg-12">
+                <div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control" required data-error="Please enter email" placeholder="password">
+                </div>
+            </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <div class="col-12">
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
+                </div>
+            </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <div class="col-lg-12 col-md-12 text-center">
+                <button type="submit" class="default-btn btn-bg-three border-radius-5">
+                    Sign Up
+                </button>
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <div class="col-12">
+                <p class="account-desc">
+                    Already have an account? 
+                    <a href="{{ route('login') }}">Sign In</a>
+                </p>
+            </div>
         </div>
     </form>
-</x-guest-layout>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Sign Up Area End -->
+
+
+
+@endsection
