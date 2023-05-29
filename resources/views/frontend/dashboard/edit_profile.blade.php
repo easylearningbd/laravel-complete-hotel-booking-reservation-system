d6c58c720ae0d17cf9fc665d2fa25cae5bd7e9d0
easylearningbd <elebdsoft@gmail.com>
2023-05-30 03:39:19 +0600

    32 User Profile Design Part 2

--- resources/views/frontend/dashboard/edit_profile.blade.php
+++ resources/views/frontend/dashboard/edit_profile.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.main_master')
 @section('main')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
   <!-- Inner Banner -->
   <div class="inner-banner inner-bg6">
@@ -26,7 +27,7 @@
              <div class="col-lg-3">
 
                 @include('frontend.dashboard.user_menu')
-                 
+
             </div>
 
 
@@ -44,56 +45,48 @@
 
                         <div class="row">
                            
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>First Name <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Name <span class="required">*</span></label>
+                <input type="text" name="name"  class="form-control" value="{{ $profileData->name }}">
+            </div>
+        </div>
 
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Last Name <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Email <span class="required">*</span></label>
+                <input type="email" name="email" class="form-control" value="{{ $profileData->email }}">
+            </div>
+        </div>
 
-                            <div class="col-lg-12 col-md-12">
-                                <div class="form-group">
-                                    <label>Company Name</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+         
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label> Address <span class="required">*</span></label>
+                <input type="text" name="adress" class="form-control" value="{{ $profileData->adress }}">
+            </div>
+        </div>
 
-                           
-
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Email Address <span class="required">*</span></label>
-                                    <input type="email" class="form-control">
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Phone <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Phone <span class="required">*</span></label>
+                <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}">
+            </div>
+        </div>
 
 
 
 <div class="col-lg-12 col-md-6">
 <div class="form-group">
     <label>User Profile  <span class="required">*</span></label>
-    <input type="file" class="form-control">
+    <input type="file" name="photo" class="form-control"  id="image">
 </div>
 </div>
 
 <div class="col-lg-12 col-md-6">
 <div class="form-group">
-    <label>Town / City <span class="required">*</span></label>
-    <input type="text" class="form-control">
+    <label>  <span class="required"> </span></label>
+    <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
 </div>
 </div>
 
@@ -117,7 +110,19 @@
 <!-- Service Details Area End -->
 
 
+<script type="text/javascript">
 
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+    </script>      
 
 
 
--- resources/views/frontend/dashboard/user_menu.blade.php
+++ resources/views/frontend/dashboard/user_menu.blade.php
@@ -1,9 +1,18 @@
+@php
+$id = Auth::user()->id;
+$profileData = App\Models\User::find($id);
+@endphp
+
 <div class="service-side-bar"> 
     <div class="services-bar-widget">
         <h3 class="title">User Sidebar </h3>
         <div class="side-bar-categories">
-<img src="{{ asset('frontend/assets/img/blog/blog-profile1.jpg') }}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;"> <br><br>
-
+<img src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;">
+<center>
+<b> {{ $profileData->name }}</b><br> 
+<b>{{ $profileData->email }}</b>
+</center>
+<br>
 <ul> 
 
 <li>
