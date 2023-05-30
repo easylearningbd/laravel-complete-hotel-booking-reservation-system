6eaec5fa592243f67acbfd6f8dc9f3127e4565f6
easylearningbd <elebdsoft@gmail.com>
2023-05-31 01:40:32 +0600

    37 User Login and Logout Notification

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Models\User;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -29,6 +30,15 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        $username = $profileData->name;
+
+        $notification = array(
+            'message' => 'User '.$username.' Login Successfully',
+            'alert-type' => 'info'
+        );
+
         $url = '';
         if($request->user()->role === 'admin'){
             $url = '/admin/dashboard';
@@ -36,7 +46,7 @@ class AuthenticatedSessionController extends Controller
             $url = '/dashboard';
         }
 
-        return redirect()->intended($url);
+        return redirect()->intended($url)->with($notification);
     }
 
     /**
