a7f36ca799984827088f411f8983dbeeeade4df3
easylearningbd <elebdsoft@gmail.com>
2023-06-01 02:00:43 +0600

    46 Add Sweetalert In Project

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -88,6 +88,8 @@
 		new PerfectScrollbar(".app-container")
 	</script>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script src="{{asset('backend/assets/js/code.js')}}"></script>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
