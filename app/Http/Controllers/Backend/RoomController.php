86e2421f9b85b03be066b034450e85b1dcb05e3c
easylearningbd <elebdsoft@gmail.com>
2023-06-04 02:43:10 +0600

    59 Edit Room Setup Part 7

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Room;
+use App\Models\Facility;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
@@ -12,8 +13,9 @@ class RoomController extends Controller
 {
     public function EditRoom($id){
 
+        $basic_facility = Facility::where('rooms_id',$id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_rooms',compact('editData'));
+        return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility'));
     } //End Method 
 
 
--- resources/views/backend/allroom/rooms/edit_rooms.blade.php
+++ resources/views/backend/allroom/rooms/edit_rooms.blade.php
@@ -119,6 +119,98 @@
             <label for="input11" class="form-label"> Description </label>
             <textarea name="description" class="form-control" id="myeditorinstance" >{!! $editData->short_desc !!}</textarea>
         </div>
+
+
+
+
+        <div class="row mt-2">
+            <div class="col-md-12 mb-3">
+               @forelse ($basic_facility as $item)
+               <div class="basic_facility_section_remove" id="basic_facility_section_remove">
+                  <div class="row add_item">
+                     <div class="col-md-8">
+                        <label for="facility_name" class="form-label"> Room Facilities </label>
+                        <select name="facility_name[]" id="facility_name" class="form-control">
+                              <option value="">Select Facility</option>
+                              <option value="Complimentary Breakfast" {{$item->facility_name == 'Complimentary Breakfast'?'selected':''}}>Complimentary Breakfast</option>
+             <option value="32/42 inch LED TV"  {{$item->facility_name == 'Complimentary Breakfast'?'selected':''}}> 32/42 inch LED TV</option>
+           
+            <option value="Smoke alarms"  {{$item->facility_name == 'Smoke alarms'?'selected':''}}>Smoke alarms</option>
+           
+            <option value="Minibar" {{$item->facility_name == 'Complimentary Breakfast'?'selected':''}}> Minibar</option>
+           
+            <option value="Work Desk"  {{$item->facility_name == 'Work Desk'?'selected':''}}>Work Desk</option>
+           
+            <option value="Free Wi-Fi" {{$item->facility_name == 'Free Wi-Fi'?'selected':''}}>Free Wi-Fi</option>
+           
+            <option value="Safety box" {{$item->facility_name == 'Safety box'?'selected':''}} >Safety box</option>
+           
+            <option value="Rain Shower" {{$item->facility_name == 'Rain Shower'?'selected':''}} >Rain Shower</option>
+           
+            <option value="Slippers" {{$item->facility_name == 'Slippers'?'selected':''}} >Slippers</option>
+           
+            <option value="Hair dryer" {{$item->facility_name == 'Hair dryer'?'selected':''}} >Hair dryer</option>
+           
+            <option value="Wake-up service"  {{$item->facility_name == 'Wake-up service'?'selected':''}}>Wake-up service</option>
+           
+            <option value="Laundry & Dry Cleaning" {{$item->facility_name == 'Laundry & Dry Cleaning'?'selected':''}} >Laundry & Dry Cleaning</option>
+            
+            <option value="Electronic door lock"  {{$item->facility_name == 'Electronic door lock'?'selected':''}}>Electronic door lock</option> 
+                        </select>
+                     </div>
+                     <div class="col-md-4">
+                        <div class="form-group" style="padding-top: 30px;">
+                              <a class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i></a>
+                              <span class="btn btn-danger btn-sm removeeventmore"><i class="fa fa-minus-circle"></i></span>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+           
+               @empty
+           
+                    <div class="basic_facility_section_remove" id="basic_facility_section_remove">
+                        <div class="row add_item">
+                            <div class="col-md-6">
+                                <label for="basic_facility_name" class="form-label">Room Facilities </label>
+                                <select name="basic_facility_name[]" id="basic_facility_name" class="form-control">
+            <option value="">Select Facility</option>
+            <option value="Complimentary Breakfast">Complimentary Breakfast</option>
+            <option value="32/42 inch LED TV" > 32/42 inch LED TV</option>
+            <option value="Smoke alarms" >Smoke alarms</option>
+            <option value="Minibar"> Minibar</option>
+            <option value="Work Desk" >Work Desk</option>
+            <option value="Free Wi-Fi">Free Wi-Fi</option>
+            <option value="Safety box" >Safety box</option>
+            <option value="Rain Shower" >Rain Shower</option>
+            <option value="Slippers" >Slippers</option>
+            <option value="Hair dryer" >Hair dryer</option>
+            <option value="Wake-up service" >Wake-up service</option>
+            <option value="Laundry & Dry Cleaning" >Laundry & Dry Cleaning</option>
+            <option value="Electronic door lock" >Electronic door lock</option> 
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group" style="padding-top: 30px;">
+                    <a class="btn btn-success addeventmore"><i class="lni lni-circle-plus"></i></a>
+           
+                   <span class="btn btn-danger removeeventmore"><i class="lni lni-circle-minus"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+           
+               @endforelse
+           
+           
+           
+                                </div> 
+                             </div>
+                             <br>
+           
+
+ 
+
  
         <div class="col-md-12">
             <div class="d-md-flex d-grid align-items-center gap-3">
@@ -222,6 +314,58 @@
      });
     });
  </script>
+
+
+<!--========== Start of add Basic Plan Facilities ==============-->
+<div style="visibility: hidden">
+    <div class="whole_extra_item_add" id="whole_extra_item_add">
+       <div class="basic_facility_section_remove" id="basic_facility_section_remove">
+          <div class="container mt-2">
+             <div class="row">
+                <div class="form-group col-md-6">
+                   <label for="basic_facility_name">Room Facilities</label>
+                   <select name="basic_facility_name[]" id="basic_facility_name" class="form-control">
+                         <option value="">Select Facility</option>
+  <option value="Complimentary Breakfast">Complimentary Breakfast</option>
+  <option value="32/42 inch LED TV" > 32/42 inch LED TV</option>
+  <option value="Smoke alarms" >Smoke alarms</option>
+  <option value="Minibar"> Minibar</option>
+  <option value="Work Desk" >Work Desk</option>
+  <option value="Free Wi-Fi">Free Wi-Fi</option>
+  <option value="Safety box" >Safety box</option>
+  <option value="Rain Shower" >Rain Shower</option>
+  <option value="Slippers" >Slippers</option>
+  <option value="Hair dryer" >Hair dryer</option>
+  <option value="Wake-up service" >Wake-up service</option>
+  <option value="Laundry & Dry Cleaning" >Laundry & Dry Cleaning</option>
+  <option value="Electronic door lock" >Electronic door lock</option> 
+                   </select>
+                </div>
+                <div class="form-group col-md-6" style="padding-top: 20px">
+                   <span class="btn btn-success addeventmore"><i class="lni lni-circle-plus"></i></span>
+                   <span class="btn btn-danger removeeventmore"><i class="lni lni-circle-minus"></i></span>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+ 
+ <script type="text/javascript">
+    $(document).ready(function(){
+       var counter = 0;
+       $(document).on("click",".addeventmore",function(){
+             var whole_extra_item_add = $("#whole_extra_item_add").html();
+             $(this).closest(".add_item").append(whole_extra_item_add);
+             counter++;
+       });
+       $(document).on("click",".removeeventmore",function(event){
+             $(this).closest("#basic_facility_section_remove").remove();
+             counter -= 1
+       });
+    });
+ </script>
+ <!--========== End of Basic Plan Facilities ==============-->
 
 
 @endsection
