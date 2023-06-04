cdcb0fea5eaf0b530dc21b65ae8ace88680d68a9
easylearningbd <elebdsoft@gmail.com>
2023-06-05 02:34:10 +0600

    66 Add Advance Room Number Setup Part 1

---
+++ database/migrations/2023_06_04_203012_create_room_numbers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rooms_id');
+            $table->integer('room_type_id');
+            $table->string('room_no')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_numbers');
+    }
+};
