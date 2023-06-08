f2d556137c8274ea948eb0624992b4aa85863768
easylearningbd <elebdsoft@gmail.com>
2023-06-09 03:09:56 +0600

    75 Show Book Area in Frontend

--- resources/views/frontend/home/room_area_two.blade.php
+++ resources/views/frontend/home/room_area_two.blade.php
@@ -1,23 +1,26 @@
+@php
+    $bookarea = App\Models\BookArea::find(1);
+@endphp
+
 <div class="book-area-two pt-100 pb-70">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-6">
                 <div class="book-content-two">
                     <div class="section-title">
-                        <span class="sp-color">MAKE A QUICK BOOKING</span>
-                        <h2>We Are the Best in All-time & So Please Get a Quick Booking</h2>
+                        <span class="sp-color">{{ $bookarea->short_title }}</span>
+                        <h2>{{ $bookarea->main_title }}</h2>
                         <p>
-                            Atoli is one of the best resorts in the global market and that's why you will get a luxury life period on the global market. We always
-                            provide you a special support for all of our guests and that's will  be the main reason to be the most popular.
+                            {{ $bookarea->short_desc }}
                         </p>
                     </div>
-                    <a href="#" class="default-btn btn-bg-three">Quick Booking</a>
+                    <a href="{{ $bookarea->link_url }}" class="default-btn btn-bg-three">Quick Booking</a>
                 </div>
             </div>
 
             <div class="col-lg-6">
                 <div class="book-img-2">
-                    <img src="{{ asset('frontend/assets/img/book-img2.jpg') }}" alt="Images">
+                    <img src="{{ asset($bookarea->image) }}" alt="Images">
                 </div>
             </div>
         </div>
