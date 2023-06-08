d49e94ae02db85e884086e18e50ffe41e6fc5379
easylearningbd <elebdsoft@gmail.com>
2023-06-09 02:49:08 +0600

    74  Show Team in Frontend Home Page

--- resources/views/frontend/home/team.blade.php
+++ resources/views/frontend/home/team.blade.php
@@ -1,3 +1,7 @@
+@php
+    $team = App\Models\Team::latest()->get();
+@endphp
+
 <div class="team-area-three pt-100 pb-70">
     <div class="container">
         <div class="section-title text-center">
@@ -5,16 +9,18 @@
             <h2>Let's Meet Up With Our Special Team Members</h2>
         </div>
         <div class="team-slider-two owl-carousel owl-theme pt-45">
+            
+            @foreach ($team as $item) 
             <div class="team-item">
                 <a href="team.html">
-                    <img src="{{ asset('frontend/assets/img/team/team-img1.jpg') }}" alt="Images">
+                    <img src="{{ asset($item->image) }}" alt="Images">
                 </a>
                 <div class="content">
-                    <h3><a href="team.html">Tom Shumate</a></h3>
-                    <span>Manager</span>
+                    <h3><a href="team.html">{{ $item->name }}</a></h3>
+                    <span>{{ $item->postion }}</span>
                     <ul class="social-link">
                         <li>
-                            <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
+                            <a href="{{ $item->facebook }}" target="_blank"><i class='bx bxl-facebook'></i></a>
                         </li> 
                         <li>
                             <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
@@ -28,78 +34,8 @@
                     </ul>
                 </div>
             </div>
-
-            <div class="team-item">
-                <a href="team.html">
-                    <img src="{{ asset('frontend/assets/img/team/team-img2.jpg') }}" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="team.html">Carrie Horton</a></h3>
-                    <span>Chief Reception Officer</span>
-                    <ul class="social-link">
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                        </li> 
-                    </ul>
-                </div>
-            </div>
-
-            <div class="team-item">
-                <a href="team.html">
-                    <img src="{{ asset('frontend/assets/img/team/team-img5.jpg') }}" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="team.html">Brian Orlando</a></h3>
-                    <span>Housekeeping</span>
-                    <ul class="social-link">
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                        </li> 
-                    </ul>
-                </div>
-            </div>
-
-            <div class="team-item">
-                <a href="team.html">
-                    <img src="{{ asset('frontend/assets/img/team/team-img4.jpg') }}" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="team.html">Michael Evens</a></h3>
-                    <span>Housekeeping</span>
-                    <ul class="social-link">
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                        </li> 
-                        <li>
-                            <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                        </li> 
-                    </ul>
-                </div>
-            </div>
+            @endforeach
+             
         </div>
     </div>
 </div>
