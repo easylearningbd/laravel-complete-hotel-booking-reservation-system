97798e20fc7de879e68139d97f3917137706d059
easylearningbd <elebdsoft@gmail.com>
2023-06-12 00:42:55 +0600

    82 Check Room Availability Part 1

--- resources/views/frontend/home/room_area.blade.php
+++ resources/views/frontend/home/room_area.blade.php
@@ -15,7 +15,7 @@
                     <div class="row align-items-center">
                         <div class="col-lg-5 col-md-4 p-0">
                             <div class="room-card-img">
-                                <a href="room-details.html">
+                                <a href="{{ url('room/details/'.$item->id) }}">
                                     <img src="{{ asset( 'upload/roomimg/'.$item->image ) }}" alt="Images">
                                 </a>
                             </div>
@@ -24,7 +24,7 @@
                         <div class="col-lg-7 col-md-8 p-0">
                             <div class="room-card-content">
                                  <h3>
-                                     <a href="room-details.html">{{ $item['type']['name'] }}</a>
+                                     <a href="{{ url('room/details/'.$item->id) }}">{{ $item['type']['name'] }}</a>
                                 </h3>
                                 <span>{{ $item->price }} / Per Night </span>
                                 <div class="rating">
