e185737c0d06a2413aa006aad2fcf8d2691eaa48
easylearningbd <elebdsoft@gmail.com>
2023-06-14 01:55:12 +0600

    93 Check Room Details Page Part 5

--- resources/views/frontend/room/search_room_details.blade.php
+++ resources/views/frontend/room/search_room_details.blade.php
@@ -1,5 +1,7 @@
 @extends('frontend.main_master')
 @section('main')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
   <!-- Inner Banner -->
   <div class="inner-banner inner-bg10">
     <div class="container">
@@ -25,7 +27,12 @@
                 <div class="room-details-side">
                     <div class="side-bar-form">
                         <h3>Booking Sheet </h3>
-<form>
+
+<form action="" method="post" id="bk_form">
+    @csrf
+    <input type="hidden" name="room_id" value="{{ $roomdetails->id }}">
+
+
     <div class="row align-items-center">
         <div class="col-lg-12">
             <div class="form-group">
@@ -320,7 +327,82 @@
 </div>
 <!-- Room Details Other End -->
 
+<script>
+    $(document).ready(function () {
+       var check_in = "{{ old('check_in') }}";
+       var check_out = "{{ old('check_out') }}";
+       var room_id = "{{ $room_id }}";
+       if (check_in != '' && check_out != ''){
+          getAvaility(check_in, check_out, room_id);
+       }
 
+
+       $("#check_out").on('change', function () {
+          var check_out = $(this).val();
+          var check_in = $("#check_in").val();
+
+          if(check_in != '' && check_out != ''){
+             getAvaility(check_in, check_out, room_id);
+          }
+       });
+
+       $(".number_of_rooms").on('change', function () {
+          var check_out = $("#check_out").val();
+          var check_in = $("#check_in").val();
+
+          if(check_in != '' && check_out != ''){
+             getAvaility(check_in, check_out, room_id);
+          }
+       });
+
+
+    });
+
+
+
+    function getAvaility(check_in, check_out, room_id) {
+       $.ajax({
+          url: "{{ route('check_room_availability') }}",
+          data: {room_id:room_id, check_in:check_in, check_out:check_out},
+          success: function(data){
+             $(".available_room").html('Availability : <span class="text-success">'+data['available_room']+' Rooms</span>');
+             $("#available_room").val(data['available_room']);
+             price_calculate(data['total_nights']);
+          }
+       });
+    }
+
+    function price_calculate(total_nights){
+       var room_price = $("#room_price").val();
+       var discount_p = $("#discount_p").val();
+       var select_room = $("#select_room").val();
+
+       var sub_total = room_price * total_nights * parseInt(select_room);
+
+       var discount_price = (parseInt(discount_p)/100)*sub_total;
+
+       $(".t_subtotal").text(sub_total);
+       $(".t_discount").text(discount_price);
+       $(".t_g_total").text(sub_total-discount_price);
+
+    }
+
+    $("#bk_form").on('submit', function () {
+       var av_room = $("#available_room").val();
+       var select_room = $("#select_room").val();
+       if (parseInt(select_room) >  av_room){
+          alert('Sorry, you select maximum number of room');
+          return false;
+       }
+       var nmbr_person = $("#nmbr_person").val();
+       var total_adult = $("#total_adult").val();
+       if(parseInt(nmbr_person) > parseInt(total_adult)){
+          alert('Sorry, you select maximum number of person');
+          return false;
+       }
+
+    })
+ </script>
 
 
 @endsection
