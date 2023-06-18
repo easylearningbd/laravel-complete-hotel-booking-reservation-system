8bbe9b1b10e0bc5ce882ad5d3f26e3767907ae72
easylearningbd <elebdsoft@gmail.com>
2023-06-19 03:28:10 +0600

    110 Backend Booking Request Part 4

--- resources/views/backend/booking/edit_booking.blade.php
+++ resources/views/backend/booking/edit_booking.blade.php
@@ -104,60 +104,46 @@
 
     <div class="row">
        <div class="col-12 col-lg-8 d-flex">
-          <div class="card radius-10 w-100">
-            <div class="card-header">
-                <div class="d-flex align-items-center">
-                    <div>
-                        <h6 class="mb-0">Sales Overview</h6>
-                    </div>
-                    <div class="dropdown ms-auto">
-                        <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="javascript:;">Action</a>
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Another action</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Something else here</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-              <div class="card-body">
-                <div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
-                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #14abef"></i>Sales</span>
-                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffc107"></i>Visits</span>
-                </div>
-                <div class="chart-container-1">
-                    <canvas id="chart1"></canvas>
-                  </div>
-              </div>
-              <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">24.15M</h5>
-                    <small class="mb-0">Overall Visitor <span> <i class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">12:38</h5>
-                    <small class="mb-0">Visitor Duration <span> <i class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">639.82</h5>
-                    <small class="mb-0">Pages/Visit <span> <i class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
-                  </div>
-                </div>
-              </div>
+          <div class="card radius-10 w-100"> 
+          <div class="card-body">
+              <div class="table-responsive">
+                <table class="table align-middle mb-0">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Room Type</th>
+                            <th>Total Room</th>
+                            <th>Price</th>
+                            <th>Check In / Out Date</th>
+                            <th>Total Days</th>
+                            <th>Total </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $editData->room->type->name }}</td>
+                            <td>{{ $editData->number_of_rooms }}</td>
+                            <td>${{ $editData->actual_price }}</td>
+                            <td>
+                                <span class="badge bg-primary">{{ $editData->check_in }}</span>  /<br> 
+                                <span class="badge bg-warning text-dark">{{ $editData->check_out }}</span></td>
+                            <td>{{ $editData->total_night }}</td>
+                            <td>${{ $editData->actual_price *  $editData->number_of_rooms }}</td>
+
+                        </tr>
+                    </tbody>
+
+                </table>
+                </div>  
+                
+               
+            </div>  
           </div>
        </div>
+
+
+
+
+
        <div class="col-12 col-lg-4 d-flex">
            <div class="card radius-10 w-100">
             <div class="card-header">
