55127722a85dcf262d76f7bea40dddfb13c31886
easylearningbd <elebdsoft@gmail.com>
2023-06-20 04:28:13 +0600

    120 Backend Booking Request Part 14

--- app/Http/Controllers/Frontend/BookingController.php
+++ app/Http/Controllers/Frontend/BookingController.php
@@ -233,6 +233,7 @@ class BookingController extends Controller
         $data->check_out = date('Y-m-d', strtotime($request->check_out));
         $data->save();
 
+        BookingRoomList::where('booking_id', $id)->delete();
         RoomBookedDate::where('booking_id', $id)->delete();
 
         $sdate = date('Y-m-d',strtotime($request->check_in ));
@@ -282,7 +283,26 @@ class BookingController extends Controller
         $check_data = BookingRoomList::where('booking_id',$booking_id)->count();
 
         if ($check_data < $booking->number_of_rooms) {
-            # code...
+           $assign_data = new BookingRoomList();
+           $assign_data->booking_id = $booking_id;
+           $assign_data->room_id = $booking->rooms_id;
+           $assign_data->room_number_id = $room_number_id;
+           $assign_data->save();
+
+           $notification = array(
+            'message' => 'Room Assign Successfully',
+            'alert-type' => 'success'
+        ); 
+        return redirect()->back()->with($notification);   
+
+        }else {
+
+            $notification = array(
+                'message' => 'Room Already Assign',
+                'alert-type' => 'error'
+            ); 
+            return redirect()->back()->with($notification);   
+
         }
 
      }// End Method 
