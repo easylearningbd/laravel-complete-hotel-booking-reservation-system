6ed948cba675b773ed32e6a39c01bf361dd014da
easylearningbd <elebdsoft@gmail.com>
2023-06-21 03:38:28 +0600

    126 Manage Room Inventory in Room List Part 3

--- app/Http/Controllers/Backend/RoomListController.php
+++ app/Http/Controllers/Backend/RoomListController.php
@@ -20,7 +20,7 @@ class RoomListController extends Controller
     public function ViewRoomList(){
 
          $room_number_list = RoomNumber::with(['room_type','last_booking.booking:id,check_in,check_out,status,code,name,phone'])->orderBy('room_type_id','asc')
-         ->leftJoin('room_types','room_types.id','room_numbers','room_type_id')
+         ->leftJoin('room_types','room_types.id','room_numbers.room_type_id')
          ->leftJoin('booking_room_lists','booking_room_lists.room_number_id','room_numbers.id')
          ->leftJoin('bookings','bookings.id','booking_room_lists.booking_id')
          ->select(
---
+++ resources/views/backend/allroom/roomlist/view_roomlist.blade.php
@@ -0,0 +1,112 @@
+@extends('admin.admin_dashboard')
+@section('admin') 
+
+<div class="page-content"> 
+	<!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+         
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">All Room List </li>
+                </ol>
+            </nav>
+        </div>
+        <div class="ms-auto">
+            <div class="btn-group">
+                <a href="{{ route('add.team') }}" class="btn btn-primary px-5">Add Booking </a>
+                
+            </div>
+        </div>
+    </div>
+    <!--end breadcrumb-->
+
+
+    
+    <hr/>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Sl</th>
+                            <th>Room Type</th>
+                            <th>Room Number</th>
+                            <th>B Status </th>
+                            <th>In/Out Date</th>
+                            <th>Booking No</th>
+                            <th>Customer</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       @foreach ($room_number_list as $key=> $item ) 
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td> {{ $item->name }}  </td>
+                            <td>{{ $item->room_no }}</td>
+                            <td>
+                            @if ($item->booking_id != '')
+                                @if ($item->booking_stauts == 1)
+                                <span class="badge bg-danger">Booked</span>
+                                 @else   
+                                 <span class="badge bg-warning">Pending</span>
+                                @endif 
+                                @else
+                                <span class="badge bg-success">Available</span>
+                            @endif    
+                            </td>
+
+                            <td>
+                             @if ($item->booking_id != '')
+                             <span class="badge rounded-pill bg-secondary">
+                                {{ date('d-m-Y', strtotime($item->check_in)) }}
+                             </span>
+                                 to 
+                                 <span class="badge rounded-pill bg-info text-dark">
+                                    {{ date('d-m-Y', strtotime($item->check_out)) }}
+                                 </span>
+                             @endif   
+                            </td>
+
+                            <td>
+                        @if ($item->booking_id != '')
+                        {{ $item->booking_no }} 
+                        @endif 
+                            </td>
+
+                             <td>
+                                @if ($item->booking_id != '')
+                                {{ $item->customer_name }} 
+                                @endif 
+                              </td>
+
+                              <td>
+                                @if ($item->status == 'Active')
+                                <span class="badge bg-success">Published</span>
+                                  @else
+                                  <span class="badge bg-danger">InActive</span>
+                                @endif
+                              </td> 
+
+                        </tr>
+                        @endforeach 
+                      
+                    </tbody>
+                 
+                </table>
+            </div>
+        </div>
+    </div>
+     
+    <hr/>
+     
+</div>
+
+
+
+
+@endsection
