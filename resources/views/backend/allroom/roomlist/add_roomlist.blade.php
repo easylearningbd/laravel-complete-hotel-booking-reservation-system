ad624cd468c9cf5cdda375d7ae0b419145b7aa3f
easylearningbd <elebdsoft@gmail.com>
2023-06-22 01:51:08 +0600

    128 Add Guest Booking  Part 2

--- resources/views/backend/allroom/roomlist/add_roomlist.blade.php
+++ resources/views/backend/allroom/roomlist/add_roomlist.blade.php
@@ -31,82 +31,84 @@
     <form class="row g-3">
         <div class="col-md-4">
             <label for="input1" class="form-label">Room Type</label>
-            <select id="input7" class="form-select">
+            <select name="room_id" id="input7" class="form-select">
                 <option selected="">Select Room Type </option>
                 @foreach ($roomtype as $item) 
-                <option>{{ $item->name }}</option>
+                <option value="{{ $item->room->id }}">{{ $item->name }}</option>
                 @endforeach
             </select>
         </div>
         <div class="col-md-4">
             <label for="input2" class="form-label">Checkin</label>
-            <input type="text" class="form-control" id="input2" placeholder="Last Name">
+     <input type="date" name="check_in" class="form-control" id="check_in"  >
         </div>
 
         <div class="col-md-4">
             <label for="input2" class="form-label">CheckOut</label>
-            <input type="text" class="form-control" id="input2" placeholder="Last Name">
+            <input type="date" name="check_out"  class="form-control" id="check_out" >
         </div>
 
 
 
-        <div class="col-md-12">
-            <label for="input3" class="form-label">Phone</label>
-            <input type="text" class="form-control" id="input3" placeholder="Phone">
-        </div>
-        <div class="col-md-12">
-            <label for="input4" class="form-label">Email</label>
-            <input type="email" class="form-control" id="input4" placeholder="Email">
-        </div>
-        <div class="col-md-12">
-            <label for="input5" class="form-label">Password</label>
-            <input type="password" class="form-control" id="input5" placeholder="Password">
-        </div>
-        <div class="col-md-12">
-            <label for="input6" class="form-label">DOB</label>
-            <input type="date" class="form-control" id="input6" placeholder="Date of Birth">
-        </div>
-        <div class="col-md-12">
-            <label for="input7" class="form-label">Country</label>
-            <select id="input7" class="form-select">
-                <option selected="">Choose...</option>
-                <option>One</option>
-                <option>Two</option>
-                <option>Three</option>
-            </select>
-        </div>
-        
-        <div class="col-md-6">
-            <label for="input8" class="form-label">City</label>
-            <input type="text" class="form-control" id="input8" placeholder="City">
-        </div>
         <div class="col-md-4">
-            <label for="input9" class="form-label">State</label>
-            <select id="input9" class="form-select">
-                <option selected="">Choose...</option>
-                <option>One</option>
-                <option>Two</option>
-                <option>Three</option>
-            </select>
+            <label for="input3" class="form-label">Room</label>
+            <input type="number" name="number_of_rooms" class="form-control" >
+
+            <input type="hidden" name="available_room" class="form-control" >
+            <div class="mt-2">
+                <label for="">Availability <span class="text-success availability"></span> </label>
+            </div>
+
         </div>
-        <div class="col-md-2">
-            <label for="input10" class="form-label">Zip</label>
-            <input type="text" class="form-control" id="input10" placeholder="Zip">
+
+
+        <div class="col-md-4">
+            <label for="input4" class="form-label">Guest</label>
+            <input type="text" name="number_of_person" class="form-control" id="number_of_person"  >
         </div>
+
+     <h3 class="mt-3 mb-5 text-center">Customer Information </h3>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">Name</label>
+            <input type="text" name="name" class="form-control" id="input5" value="{{ old('name') }}"  >
+        </div>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control"   value="{{ old('email') }}"  >
+        </div>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">Phone</label>
+            <input type="text" name="phone" class="form-control" value="{{ old('phone') }}"  >
+        </div>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">Country</label>
+            <input type="text" name="country" class="form-control" value="{{ old('country') }}"  >
+        </div>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">Zip Code</label>
+            <input type="text" name="zip_code" class="form-control" value="{{ old('zip_code') }}"  >
+        </div>
+
+        <div class="col-md-4">
+            <label for="input5" class="form-label">State</label>
+            <input type="text" name="state" class="form-control" value="{{ old('state') }}"  >
+        </div>
+         
+       
+       
         <div class="col-md-12">
             <label for="input11" class="form-label">Address</label>
-            <textarea class="form-control" id="input11" placeholder="Address ..." rows="3"></textarea>
+            <textarea name="address" class="form-control"  rows="3">{{ old('address') }}</textarea>
         </div>
-        <div class="col-md-12">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="input12">
-                <label class="form-check-label" for="input12">Check me out</label>
-            </div>
-        </div>
+         
         <div class="col-md-12">
             <div class="d-md-flex d-grid align-items-center gap-3">
-                <button type="button" class="btn btn-primary px-4">Submit</button>
-                <button type="button" class="btn btn-light px-4">Reset</button>
+                <button type="submit" class="btn btn-primary px-4">Submit</button> 
             </div>
         </div>
     </form>
