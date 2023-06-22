5bf0d128c28b1878b03f04e1e9d82218026a9490
easylearningbd <elebdsoft@gmail.com>
2023-06-23 04:14:24 +0600

    140 Dynamic Email Configuration Part 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\SmtpSetting;
+use Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (\Schema::hasTable('smtp_settings')) {
+           $smtpsetting = SmtpSetting::first();
+
+           if ($smtpsetting) {
+              $data = [
+                'driver' => $smtpsetting->mailer, 
+                'host' => $smtpsetting->host,
+                'port' => $smtpsetting->port,
+                'username' => $smtpsetting->username,
+                'password' => $smtpsetting->password,
+                'encryption' => $smtpsetting->encryption,
+                'from' => [
+                    'address' => $smtpsetting->from_address,
+                    'name' => 'Easyhotel'
+                ]  
+              ];
+              Config::set('mail',$data);
+           }
+
+        } // end if
+        
+
+
+
+
+
+
     }
-}
+} 
