f9d436d532c931f2340a479bc53ea6125bc0aa31
easylearningbd <elebdsoft@gmail.com>
2023-06-28 03:43:07 +0600

    154 Show Blog in frontend Page Part 1

--- resources/views/frontend/home/blog.blade.php
+++ resources/views/frontend/home/blog.blade.php
@@ -1,3 +1,6 @@
+@php
+    $blog = App\Models\BlogPost::latest()->limit(3)->get();
+@endphp
 <div class="blog-area pt-100 pb-70">
     <div class="container">
         <div class="section-title text-center">
@@ -5,71 +8,34 @@
             <h2>Our Latest Blogs to the Intranational Journal at a Glance</h2>
         </div>
         <div class="row pt-45">
+
+            @foreach ($blog as $item ) 
+          
             <div class="col-lg-4 col-md-6">
                 <div class="blog-item">
                     <a href="blog-details.html">
-                        <img src="{{ asset('frontend/assets/img/blog/blog-item-img1.jpg') }}" alt="Images">
+                        <img src="{{ asset($item->post_image) }}" alt="Images">
                     </a>
                     <div class="content">
                         <ul>
-                            <li>October 01, 2020</li>
+                            <li>{{ $item->created_at->format('M d Y')  }}</li>
                             <li><i class='bx bx-user'></i>29K</li>
                             <li><i class='bx bx-message-alt-dots'></i>15K</li>
                         </ul>
                         <h3>
-                            <a href="blog-details.html">Hotel Management is the Best Policy</a>
+                            <a href="blog-details.html">{{ $item->post_titile }}</a>
                         </h3>
-                        <p>This is one of the best & quality full hotels in the world that will help you to make an excellent study market.</p>
+                        <p>{{ $item->short_descp }}</p>
                         <a href="blog-details.html" class="read-btn">
                             Read More
                         </a>
                     </div>
                 </div>
             </div>
+            @endforeach
 
-            <div class="col-lg-4 col-md-6">
-                <div class="blog-item">
-                    <a href="blog-details.html">
-                        <img src="{{ asset('frontend/assets/img/blog/blog-item-img2.jpg') }}" alt="Images">
-                    </a>
-                    <div class="content">
-                        <ul>
-                            <li>October 07, 2020</li>
-                            <li><i class='bx bx-user'></i>22K</li>
-                            <li><i class='bx bx-message-alt-dots'></i>24K</li>
-                        </ul>
-                        <h3>
-                            <a href="blog-details.html">3d Hotel Models Have an Important Model</a>
-                        </h3>
-                        <p>This is one of the best & quality full hotels in the world that will help you to make an excellent study market.</p>
-                        <a href="blog-details.html" class="read-btn">
-                            Read More
-                        </a>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-                <div class="blog-item">
-                    <a href="blog-details.html">
-                        <img src="{{ asset('frontend/assets/img/blog/blog-item-img3.jpg') }}" alt="Images">
-                    </a>
-                    <div class="content">
-                        <ul>
-                            <li>October 17, 2020</li>
-                            <li><i class='bx bx-user'></i>27K</li>
-                            <li><i class='bx bx-message-alt-dots'></i>39K</li>
-                        </ul>
-                        <h3>
-                            <a href="blog-details.html">Hotel Management Has a Good Future Era</a>
-                        </h3>
-                        <p>This is one of the best & quality full hotels in the world that will help you to make an excellent study market.</p>
-                        <a href="blog-details.html" class="read-btn">
-                            Read More
-                        </a>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 </div>
