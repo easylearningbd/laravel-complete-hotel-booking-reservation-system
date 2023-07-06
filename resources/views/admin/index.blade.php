dff9622e312cf1f6be29cef104b1795f93bf4eae
easylearningbd <elebdsoft@gmail.com>
2023-07-07 04:41:15 +0600

    183 Custom Admin Dashboard Part 3

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,7 @@
 @extends('admin.admin_dashboard')
 @section('admin') 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 @php
   $bookings = App\Models\Booking::latest()->get();
@@ -79,102 +81,28 @@
     </div><!--end row-->
 
     <div class="row">
-       <div class="col-12 col-lg-8 d-flex">
+       <div class="col-12 col-lg-12 d-flex">
           <div class="card radius-10 w-100">
             <div class="card-header">
                 <div class="d-flex align-items-center">
                     <div>
                         <h6 class="mb-0">Sales Overview</h6>
                     </div>
-                    <div class="dropdown ms-auto">
-                        <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="javascript:;">Action</a>
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Another action</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Something else here</a>
-                            </li>
-                        </ul>
-                    </div>
+                     
                 </div>
             </div>
-              <div class="card-body">
-                <div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
-                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #14abef"></i>Sales</span>
-                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffc107"></i>Visits</span>
-                </div>
-                <div class="chart-container-1">
-                    <canvas id="chart1"></canvas>
-                  </div>
-              </div>
+            
               <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">24.15M</h5>
-                    <small class="mb-0">Overall Visitor <span> <i class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">12:38</h5>
-                    <small class="mb-0">Visitor Duration <span> <i class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="p-3">
-                    <h5 class="mb-0">639.82</h5>
-                    <small class="mb-0">Pages/Visit <span> <i class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
-                  </div>
-                </div>
+                 
+               <canvas id="bookingChart"></canvas>
+               
               </div>
           </div>
        </div>
-       <div class="col-12 col-lg-4 d-flex">
-           <div class="card radius-10 w-100">
-            <div class="card-header">
-                <div class="d-flex align-items-center">
-                    <div>
-                        <h6 class="mb-0">Trending Products</h6>
-                    </div>
-                    <div class="dropdown ms-auto">
-                        <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="javascript:;">Action</a>
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Another action</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Something else here</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-               <div class="card-body">
-                <div class="chart-container-2">
-                    <canvas id="chart2"></canvas>
-                  </div>
-               </div>
-               <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center border-top">Jeans <span class="badge bg-success rounded-pill">25</span>
-                </li>
-                <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">T-Shirts <span class="badge bg-danger rounded-pill">10</span>
-                </li>
-                <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">Shoes <span class="badge bg-primary rounded-pill">65</span>
-                </li>
-                <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">Lingerie <span class="badge bg-warning text-dark rounded-pill">14</span>
-                </li>
-            </ul>
-           </div>
-       </div>
+       
+
+
+
     </div><!--end row-->
 
      <div class="card radius-10">
@@ -226,11 +154,44 @@
 
 
         </div>
-
-
-       
-
+ 
          
 </div>
+
+
+<script>
+  var ctx = document.getElementById('bookingChart').getContext('2d');
+  var bookings = @json($bookings);
+
+  // Extract the required data from the bookings
+  var labels = bookings.map(function(booking) {
+      return booking.check_in; 
+  });
+
+  var data = bookings.map(function(booking) {
+      return booking.total_price;
+  });
+
+  var bookingChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: labels,
+          datasets: [{
+              label: 'Booking Data',
+              data: data,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+</script>
 
 @endsection
