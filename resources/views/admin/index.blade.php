3900dce7e2ab380b370cb84fae04336cd36833d6
easylearningbd <elebdsoft@gmail.com>
2023-07-07 04:27:55 +0600

    182 Custom Admin Dashboard Part 2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,6 +10,8 @@
   $today = Carbon\Carbon::now()->toDateString();
   $todayprice = App\Models\Booking::whereDate('created_at',$today)->sum('total_price');
 
+  $allData = App\Models\Booking::orderBy('id','desc')->limit(10)->get();
+
 @endphp
 
 <div class="page-content">
@@ -179,112 +181,50 @@
         <div class="card-header">
             <div class="d-flex align-items-center">
                 <div>
-                    <h6 class="mb-0">Recent Orders</h6>
+                    <h6 class="mb-0">Recent Booking</h6>
                 </div>
-                <div class="dropdown ms-auto">
-                    <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="javascript:;">Action</a>
-                        </li>
-                        <li><a class="dropdown-item" href="javascript:;">Another action</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="javascript:;">Something else here</a>
-                        </li>
-                    </ul>
-                </div>
+                
             </div>
         </div>
-             <div class="card-body">
-             <div class="table-responsive">
-               <table class="table align-middle mb-0">
-                <thead class="table-light">
-                 <tr>
-                   <th>Product</th>
-                   <th>Photo</th>
-                   <th>Product ID</th>
-                   <th>Status</th>
-                   <th>Amount</th>
-                   <th>Date</th>
-                   <th>Shipping</th>
-                 </tr>
-                 </thead>
-                 <tbody><tr>
-                  <td>Iphone 5</td>
-                  <td><img src="assets/images/products/01.png" class="product-img-2" alt="product img"></td>
-                  <td>#9405822</td>
-                  <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-                  <td>$1250.00</td>
-                  <td>03 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-                      </div></td>
-                 </tr>
 
-                 <tr>
-                  <td>Earphone GL</td>
-                  <td><img src="assets/images/products/02.png" class="product-img-2" alt="product img"></td>
-                  <td>#8304620</td>
-                  <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-                  <td>$1500.00</td>
-                  <td>05 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-                      </div></td>
-                 </tr>
-
-                 <tr>
-                  <td>HD Hand Camera</td>
-                  <td><img src="assets/images/products/03.png" class="product-img-2" alt="product img"></td>
-                  <td>#4736890</td>
-                  <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-                  <td>$1400.00</td>
-                  <td>06 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 70%"></div>
-                      </div></td>
-                 </tr>
-
-                 <tr>
-                  <td>Clasic Shoes</td>
-                  <td><img src="assets/images/products/04.png" class="product-img-2" alt="product img"></td>
-                  <td>#8543765</td>
-                  <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-                  <td>$1200.00</td>
-                  <td>14 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-                      </div></td>
-                 </tr>
-                 <tr>
-                  <td>Sitting Chair</td>
-                  <td><img src="assets/images/products/06.png" class="product-img-2" alt="product img"></td>
-                  <td>#9629240</td>
-                  <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-                  <td>$1500.00</td>
-                  <td>18 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-                      </div></td>
-                 </tr>
-                 <tr>
-                  <td>Hand Watch</td>
-                  <td><img src="assets/images/products/05.png" class="product-img-2" alt="product img"></td>
-                  <td>#8506790</td>
-                  <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-                  <td>$1800.00</td>
-                  <td>21 Feb 2020</td>
-                  <td><div class="progress" style="height: 6px;">
-                        <div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 40%"></div>
-                      </div></td>
-                 </tr>
-                </tbody>
+        <div class="card-body">
+          <div class="table-responsive">
+              <table class="table table-striped table-bordered" style="width:100%">
+                  <thead>
+                      <tr>
+                          <th>Sl</th>
+                          <th>B No</th>
+                          <th>B Date</th>
+                          <th>Customer</th>
+                          <th>Room</th>
+                          <th>Check IN/Out</th>
+                          <th>Total Room</th>
+                          <th>Guest</th> 
+                      </tr>
+                  </thead>
+                  <tbody>
+                     @foreach ($allData as $key=> $item ) 
+                      <tr>
+                          <td>{{ $key+1 }}</td>
+                          <td> <a href="{{ route('edit_booking',$item->id) }}"> {{ $item->code }} </a></td>
+                          <td> {{ $item->created_at->format('d/m/Y') }} </td>
+                          <td> {{ $item['user']['name'] }} </td>
+                          <td> {{ $item['room']['type']['name'] }} </td>
+                          <td> <span class="badge bg-primary">{{ $item->check_in }}</span>   <span class="badge bg-warning text-dark">{{ $item->check_out }}</span> </td>
+                          <td> {{ $item->number_of_rooms }} </td>
+                          <td> {{ $item->persion }} </td>
+                    
+                           
+                      </tr>
+                      @endforeach 
+                    
+                  </tbody>
+               
               </table>
-              </div>
-             </div>
+          </div>
+      </div>
+
+
         </div>
 
 
