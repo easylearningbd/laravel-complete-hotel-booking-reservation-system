192a27e39fe84c9792e26fe09d5d1dcc8f492cbe
easylearningbd <elebdsoft@gmail.com>
2023-07-08 02:10:33 +0600

    187 Install Laravel Spatie Permission

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
